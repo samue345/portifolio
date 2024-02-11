15d8bf634ca8aac8717d770d68455e9c95f53866
Samuel Souza <85644942+samue345@users.noreply.github.com>
2024-02-11 01:12:06 -0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll('[data-tab-value]');
+    const tabInfos = document.querySelectorAll('[data-tab-info]');
+    const tabsList = document.querySelector('.tabs');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.tabValue);
+            tabs.forEach(t => t.classList.remove('active'));
+            tabInfos.forEach(tabInfo => tabInfo.classList.remove('active'));
+            tab.classList.add('active');
+            target.classList.add('active');
+            tabsList.querySelectorAll('li').forEach(li => {
+                li.style.borderBottom = 'none';
+            });
+
+            tab.style.borderBottom = '3px solid #fff';
+        });
+    });
+
+    tabs[0].click();
+});
